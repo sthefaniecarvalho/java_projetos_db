11bec82481aed00b46b7d64725b9a33405055d19
Sthefanie <sthefaniegc06@gmail.com>
2022-11-02 11:09:51 -0300

    First commit

---
+++ jbase/Programa.java
@@ -0,0 +1,14 @@
+package jbase;
+
+import java.util.Locale;
+
+public class Programa {
+	
+	
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Utils.menu();
+	}
+}
