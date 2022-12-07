e07efcf8dbac916d03657e7e980274969dd3f5c1
Sthefanie <sthefaniegc06@gmail.com>
2022-12-07 13:09:43 -0300

    jcouch

---
+++ jcouch/Programa.java
@@ -0,0 +1,16 @@
+package jcouch;
+
+import java.util.Locale;
+
+public class Programa {
+	
+	
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Utils.menu();
+	}
+}
+
+// 2-68119e4af85c6267a0e1733fb7b66deb
---
+++ jfirebase/Utils.java
@@ -0,0 +1,58 @@
+
+package jfirebase;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class Utils {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void conectar() {
+		System.out.println("Conectando");
+	}
+
+	public static void desconectar(Connection conn){
+		System.out.println("Desconectando");
+	}
+
+	public static void listar() {
+		System.out.println("Listando..");
+	
+	}
+	
+	public static void inserir() {
+		System.out.println("Inserindo...");
+	}
+
+	public static void atualizar() {
+		System.out.println("Atualizando..");
+	}
+
+	public static void deletar() {
+		System.out.println("Deletando");
+		
+	}
+
+	public static void menu() {
+		System.out.println("==================Gerenciamento de Produtos===============");
+		System.out.println("Selecione uma opção: ");
+		System.out.println("1 - Listar produtos.");
+		System.out.println("2 - Inserir produtos.");
+		System.out.println("3 - Atualizar produtos.");
+		System.out.println("4 - Deletar produtos.");
+		
+		int opcao = Integer.parseInt(sc.nextLine());
+		if(opcao == 1) {
+			listar();
+		}else if(opcao == 2) {
+			inserir();
+		}else if(opcao == 3) {
+			atualizar();
+		}else if(opcao == 4) {
+			deletar();
+		}else {
+			System.out.println("Opção inválida.");
+		}
+	}
+}
